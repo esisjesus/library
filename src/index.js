986c738cc68717eef7cba8b0779ceea6ff1f3aaa
Jesus <jesisp17@gmail.com>
2021-11-21 19:41:29 -0300

    setup google sign in with firebase's google provider

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ signInForm.addEventListener('submit', (e) => {
         .then((userCredential) => {
             // Signed in
             var user = userCredential.user;
+            console.log(user)
             // Close and clear form
             signInForm.reset()
             signInModal.hide()
@@ -46,10 +47,41 @@ signInForm.addEventListener('submit', (e) => {
         });
 })
 
+// GOOGLE SIGN IN FUNCTION:
+const googleButtons = document.querySelectorAll("#googleSignIn")
+googleButtons.forEach(e=>{
+    e.addEventListener('click', (e) => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider)
+            .then(result => {
+                console.log(result.user, result.credential)
+            }).catch(err => {
+                console.log(err)
+            })
+        signUpForm.reset()
+        signUpModal.hide()
+        signInForm.reset()
+        signInModal.hide()
+    })
+})
+
 // LOGOUT EVENT: 
 logout.addEventListener("click", (e) => {
     e.preventDefault();
-    auth.signOut.then(()=>{
+    auth.signOut().then(() => {
         console.log("logged out")
     })
+})
+
+// CHECK USER
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        logout.style.display = "block"
+        document.querySelector("#signIn").style.display = "none"
+        document.querySelector("#signUp").style.display = "none"
+    } else {
+        logout.style.display = "none"
+        document.querySelector("#signIn").style.display = "block"
+        document.querySelector("#signUp").style.display = "block"
+    }
 })
